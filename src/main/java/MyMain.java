c148b5e0a80138cc06498b0ed78e6c3f0130ccde
BrendanMcCulloch <22bmcculloch@athenian.org>
2020-09-10 06:15:17 +0000

    made 3 functions, first function I had for loop and if statement

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,26 +4,60 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int counter = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == "c") {
+                return counter++;
+            }
+            else if (str.charAt(i) == "b"){
+                return counter++;
+            }
+            else if (str.charAt(i) == "a"){
+                return counter++;
+            }
+
+        return counter;
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
+        String THE = "the";
+        if (str.toLowerCase().indexOf(THE.toLowerCase()) != -1) {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        int b = 0, a = str.length() - 1; 
+        while (b < a ){
+            if (str.charAt(b) != str.charAt(a))
+                return false;
+            a--;
+            b++;
+
+        }
+        
+        return true;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // YOUR CODE HERE
+        System.out.print("Any sentence or phrase: ");
+        String abc = scan.nextLine();
+        System.out.println(countABC(abc));
+        System.out.print("Any sentence or phrase: ");
+        String the = scan.nextLine();
+        System.out.println(containsThe(the));
+        System.out.print("A word: ");
+        String palindrome = scan.nextLine();
+        System.out.println(isPalindrome(palindrome));
+
+
     }
 }
